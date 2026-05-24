23b16e5ed53bdc7b4b1c452b63cf1ec76cae2e09
Hrishika Anil Shelar <hrishikashelar311@gmail.com>
2026-05-23 19:55:23 -0500

    fix: add SSL support for Aiven MySQL

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {"ssl_disabled": False}}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
